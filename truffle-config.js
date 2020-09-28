c412da2deff01391ca9ddbfed33b8437af600789
Tom French <tom@tomfren.ch>
2020-09-29 00:51:13 +0100

    feat: remove unused networks / values from truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,46 +1,7 @@
 require('babel-register')
 require('babel-polyfill')
 
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
-const MNEMONIC = process.env.MNEMONIC || 'clock radar mass judge dismiss just intact mind resemble fringe diary casino'
-const API_KEY = process.env.API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
     development: {
       host: 'localhost',
@@ -64,50 +25,6 @@ module.exports = {
       skipDryRun: true,
       gas: 7000000,
       gasPrice: '0'
-    },
-    mumbaiRoot: {
-      provider: () =>
-        new HDWalletProvider(
-          MNEMONIC,
-          `https://goerli.infura.io/v3/${API_KEY}`
-        ),
-      network_id: 5,
-      gas: 7000000,
-      gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true
-    },
-    mumbaiChild: {
-      provider: () =>
-        new HDWalletProvider(
-          MNEMONIC,
-          'https://rpc-mumbai.matic.today'
-        ),
-      network_id: 80001,
-      gas: 7000000,
-      gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true
-    },
-    mainnetRoot: {
-      provider: () =>
-        new HDWalletProvider(
-          MNEMONIC,
-          `https://mainnet.infura.io/v3/${API_KEY}`
-        ),
-      network_id: 1,
-      gas: 7000000,
-      gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true
-    },
-    mainnetChild: {
-      provider: () =>
-        new HDWalletProvider(
-          MNEMONIC,
-          'https://rpc-mainnet.matic.network'
-        ),
-      network_id: 137,
-      gas: 7000000,
-      gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true
     }
   },
 
@@ -136,13 +53,5 @@ module.exports = {
         evmVersion: 'istanbul'
       }
     }
-  },
-
-  verify: {
-    preamble: 'Matic PoS Portal'
-  },
-
-  api_keys: {
-    etherscan: ETHERSCAN_API_KEY
   }
 }
