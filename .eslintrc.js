6a9f56c662c327a4696429deda804a027d1b271d
Tom French <tom@tomfren.ch>
2020-09-29 14:46:42 +0100

    style: switch over to airbnb-typescript-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb/base', "plugin:prettier/recommended"],
+  extends: "airbnb-typescript-prettier",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+      "project": "./tsconfig.json",
+      "tsconfigRootDir": __dirname,
+      "sourceType": "module"
+  },
   env: {
     node: true,
     es6: true,
@@ -10,5 +15,18 @@ module.exports = {
     contract: true,
     web3: true,
     assert: true
+  },
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "ts": "never"
+      }
+    ],
+    "import/prefer-default-export": "off",
+    "prefer-destructuring": "off",
+    "prefer-template": "off",
   }
 }
