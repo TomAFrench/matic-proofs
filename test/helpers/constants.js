0c65c843d3c9a9ddfcbe29a700629b4fc237edb7
Tom French <tom@tomfren.ch>
2020-10-01 12:38:52 +0100

    feat: remove commented code

--- test/helpers/constants.js
+++ test/helpers/constants.js
@@ -1,15 +1,5 @@
 import BN from "bn.js";
 
-// export const rootRPC = 'http://localhost:8545'
-// export const childRPC = 'http://localhost:9545'
-
-// export const rootWeb3 = new web3.constructor(
-//   new web3.providers.HttpProvider(rootRPC)
-// )
-// export const childWeb3 = new web3.constructor(
-//   new web3.providers.HttpProvider(childRPC)
-// )
-
 export const rootRPC = "http://localhost:9545";
 export const childRPC = "http://localhost:8545";
 
