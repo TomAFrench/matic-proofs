5ace494a08cde2b2bdf972dfa6520c6df4c96502
Tom French <tom@tomfren.ch>
2020-10-08 15:01:52 +0100

    fix: revert back to using bluebird for limit on concurrency

--- src/proofs/blockProof.ts
+++ src/proofs/blockProof.ts
@@ -1,6 +1,6 @@
 import { JsonRpcProvider } from "@ethersproject/providers";
 import BN from "bn.js";
-
+import { map } from "bluebird";
 import { toBuffer, keccak256 } from "ethereumjs-util";
 import { BigNumber } from "@ethersproject/bignumber";
 import MerkleTree from "../utils/merkleTree";
@@ -16,13 +16,22 @@ const getBlockHeader = (block: RequiredBlockMembers): Buffer => {
 };
 
 const buildBlockHeaderMerkle = async (maticChainProvider: JsonRpcProvider, start: number, end: number) => {
-  const blocks = await Promise.all(
-    Array.from({ length: end - start + 1 }, async (_, index: number) =>
-      getFullBlockByNumber(maticChainProvider, start + index),
-    ),
+  const headers = new Array(end - start + 1);
+  await map(
+    headers,
+    // eslint-disable-next-line
+    async (_, index: number) => {
+      headers[index] = getBlockHeader(await getFullBlockByNumber(maticChainProvider, start + index));
+    },
+    { concurrency: 20 },
   );
 
-  const headers = blocks.map(block => getBlockHeader(block));
+  // const blocks = await Promise.all(
+  //   Array.from({ length: end - start + 1 }, async (_, index: number) =>
+  //     getFullBlockByNumber(maticChainProvider, start + index),
+  //   ),
+  // );
+  // const headers = blocks.map(block => getBlockHeader(block));
   return new MerkleTree(headers);
 };
 
