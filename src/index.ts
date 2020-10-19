0afe4babad29beb7962d9ed2ae5d39dd04559ba3
Tom French <tom@tomfren.ch>
2020-10-19 12:57:36 +0100

    feat: export transfer event signatures

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,11 @@ import { isBlockCheckpointed } from "./checks";
 export { isBlockCheckpointed, isBurnTxCheckpointed, isBurnTxProcessed, isBurnTxClaimable } from "./checks";
 export { getReceiptProof } from "./proofs/receiptProof";
 
+export const ERC20_TRANSFER_EVENT_SIG = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+export const ERC721_TRANSFER_EVENT_SIG = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+export const ERC1155_TRANSFER_SINGLE_EVENT_SIG = "0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62";
+export const ERC1155_TRANSFER_BATCH_EVENT_SIG = "0x4a39dc06d4c0dbc64b70af90fd698a233a518aa5d07e595d983b8c0526c8f7fb";
+
 export const encodePayload = ({
   headerBlockNumber,
   blockProof,
