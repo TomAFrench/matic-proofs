368dbf30c6b92ee0b4a02f7fb5dc61322bb761ed
Tom French <tom@tomfren.ch>
2020-10-19 13:53:00 +0100

    fix: use HashZero instead of AddressZero in getLogIndex
    Addresses are padded with preceding zeros up to the length of a hash

--- src/utils/logIndex.ts
+++ src/utils/logIndex.ts
@@ -1,11 +1,11 @@
 import { TransactionReceipt } from "@ethersproject/providers";
-import { AddressZero } from "@ethersproject/constants";
+import { HashZero } from "@ethersproject/constants";
 
 export const getLogIndex = (transactionReceipt: TransactionReceipt, logEventSig: string): number => {
   // Check topics[0] to find a transfer of the correct asset type e.g. ERC20
   // Check topics[2] to filter out any transfers previous to the burn i.e. those not to the zero address
   const logIndex = transactionReceipt.logs.findIndex(
-    log => log.topics[0].toLowerCase() === logEventSig.toLowerCase() && log.topics[2].toLowerCase() === AddressZero,
+    log => log.topics[0].toLowerCase() === logEventSig.toLowerCase() && log.topics[2].toLowerCase() === HashZero,
   );
   if (logIndex === -1) {
     throw new Error("Burn log not found in receipt");
