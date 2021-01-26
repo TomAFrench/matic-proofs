41406a51a096e51bcd89a4318397808b8d1c2358
Tom French <tom@tomfren.ch>
2021-01-26 16:21:54 +0000

    refactor: add return type to referenceReceiptBytesImplementation

--- test/utils/getReceiptBytes.ts
+++ test/utils/getReceiptBytes.ts
@@ -5,7 +5,7 @@ import { TransactionReceipt } from "@ethersproject/providers";
 
 // Implementation of getReceiptBytes as taken from Matic.js repo:
 // See: https://github.com/maticnetwork/matic.js/blob/6860dbb874b0213260cbb8e35ce81a471ddade6e/src/libs/ProofsUtil.ts#L190-L208
-export const referenceReceiptBytesImplementation = (receiptData: TransactionReceipt) =>
+export const referenceReceiptBytesImplementation = (receiptData: TransactionReceipt): Buffer =>
   rlp.encode([
     toBuffer(
       // eslint-disable-next-line no-nested-ternary
