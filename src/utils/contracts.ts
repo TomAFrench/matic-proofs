b279fa4d6e6d4c89c22d1d9bf7677f0f974f269a
Tom French <tom@tomfren.ch>
2021-01-27 20:20:43 +0000

    fix: revert back to traditional abis

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -1,15 +1,10 @@
 import { Contract } from "@ethersproject/contracts";
 import { Provider } from "@ethersproject/providers";
+import checkpointManagerABI from "../abi/ICheckpointManager.json";
+import rootChainManagerABI from "../abi/RootChainManager.json";
 
 export const getRootChainManager = (rootChainProvider: Provider, rootChainContractAddress: string): Contract =>
-  new Contract(
-    rootChainContractAddress,
-    [
-      "function checkpointManagerAddress() view returns (address)",
-      "function processedExits(bytes32) view returns (bool)",
-    ],
-    rootChainProvider,
-  );
+  new Contract(rootChainContractAddress, rootChainManagerABI, rootChainProvider);
 
 export const getCheckpointManager = async (
   rootChainProvider: Provider,
@@ -17,15 +12,7 @@ export const getCheckpointManager = async (
 ): Promise<Contract> => {
   const rootChainContract = getRootChainManager(rootChainProvider, rootChainContractAddress);
   const checkpointManagerAddress = await rootChainContract.checkpointManagerAddress();
-  const checkpointManagerContract = new Contract(
-    checkpointManagerAddress,
-    [
-      "function currentHeaderBlock() view returns (uint256)",
-      "function headerBlocks(uint256 checkpointId) view returns (tuple(bytes32 root, uint256 start, uint256 end, uint256 createdAt, address proposer) checkpoint)",
-      "function getLastChildBlock() view returns (uint256)",
-    ],
-    rootChainProvider,
-  );
+  const checkpointManagerContract = new Contract(checkpointManagerAddress, checkpointManagerABI, rootChainProvider);
 
   return checkpointManagerContract;
 };
