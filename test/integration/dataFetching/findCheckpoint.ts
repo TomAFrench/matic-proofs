e28119674dbb84830cc95d423feb48a3f32642fb
Tom French <tom@tomfren.ch>
2021-01-29 18:02:48 +0000

    test: add a test for checkpoint search falling back to binary search if no subgraph exists

--- test/integration/dataFetching/findCheckpoint.ts
+++ test/integration/dataFetching/findCheckpoint.ts
@@ -1,9 +1,14 @@
 /* eslint-disable func-names */
 import { BigNumber } from "@ethersproject/bignumber";
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { fallbackFindBlockCheckpointId, subgraphGetCheckpoint } from "../../../src/utils/checkpoint";
+import {
+  fallbackFindBlockCheckpointId,
+  getBlockCheckpoint,
+  subgraphGetCheckpoint,
+} from "../../../src/utils/checkpoint";
 import { getCheckpointManager } from "../../../src/utils/contracts";
 import chai from "../../chai-setup";
+import { block, CHECKPOINT_ID } from "../../mockResponses";
 
 const { expect } = chai;
 
@@ -15,12 +20,12 @@ const testData: [number, number][] = [
   [9840896, 97070000],
 ];
 
+const rootChainManagerProxy = "0xa0c68c638235ee32657e8f720a23cec1bfc77c77";
+
 export function testFindBlockCheckpointId(): void {
   testData.forEach(([blockNumber, expectedCheckpointId]) => {
     it(`should return ${expectedCheckpointId} as the checkpoint id for block number ${blockNumber}`, async () => {
       const provider = new JsonRpcProvider("https://mainnet.infura.io/v3/8e1441aac766490a8671d8800c334c64");
-
-      const rootChainManagerProxy = "0xa0c68c638235ee32657e8f720a23cec1bfc77c77";
       const checkpointManager = await getCheckpointManager(provider, rootChainManagerProxy);
       const checkpointId = await fallbackFindBlockCheckpointId(checkpointManager, BigNumber.from(blockNumber));
 
@@ -32,5 +37,13 @@ export function testFindBlockCheckpointId(): void {
 
       expect(checkpointId).to.eq(expectedCheckpointId);
     });
+  });
+
+  it(`should fall back to an on-chain lookup if no subgraph exists for this network`, async () => {
+    const provider = new JsonRpcProvider("https://mainnet.infura.io/v3/8e1441aac766490a8671d8800c334c64");
+    provider.getNetwork = async () => ({ name: "Bad network", chainId: 100 });
+    const { checkpointId } = await getBlockCheckpoint(provider, rootChainManagerProxy, BigNumber.from(block.number));
+
+    expect(checkpointId).to.eq(CHECKPOINT_ID);
   });
 }
